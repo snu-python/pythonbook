6098cce2214e52e7d392186eb01a58df19197456
Jinsoo Park <jinsoo@snu.ac.kr>
2019-07-12 11:33:44 +0900

    Add another way of using enumerate()

--- lab/lab8_6.py
+++ lab/lab8_6.py
@@ -16,10 +16,14 @@ __email__ = 'snu.python@gmail.com'
 member_list = ['무지', '어피치', '네오', '제이지', '콘', '프로도', '라이언', '튜브']
 print(member_list)                  # 변수 member_list를 출력한다.
 
+# --- for 순환문을 사용하지 않고 enumerate() 함수를 바로 활용해도 무방하다.
 member_dict = {}                    # 신규 회원을 담을 변수 member_dict를 초기화한다.
 
 for mem_id, name in enumerate(member_list, start=153):
     member_dict[mem_id] = name      # member_dict에 객체 쌍들을 추가한다.
+
+# --- for 순환문 사용하지 않고 enumerate() 함수만으로도 위 3줄 코드와 같은 결과를 가진다.(주석처리)
+# member_dict = dict(enumerate(member_list, start=153))
 
 print(member_dict)                  # 변수 member_dict를 출력한다.
 
