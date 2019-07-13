24fa1d08bcfdb4068a2515097db81f961bffcbf2
Jinsoo Park <jinsoo@snu.ac.kr>
2019-07-13 23:38:38 +0900

    Switch some code to increase clarity.

--- lab/lab8_7.py
+++ lab/lab8_7.py
@@ -13,12 +13,13 @@ __email__ = 'snu.python@gmail.com'
 
 
 numbers = [1, 4, 7, 3, 8, -1, 0, 5, 22, 2, 31]
+
 for i in numbers:
-    if i < 0:                                       # i가 음수면
-        break                                       # for문을 종료한다.
-    elif i % 2 == 1:                                # i가 홀수면
-        print(i)                                    # 그 값을 출력한다.
-    else:                                           # i가 짝수면
-        continue                                    # 건너뛴다.
+    if i < 0:           # i가 음수면
+        break           # for문을 종료한다.
+    elif i % 2 == 0:    # i가 짝수면
+        continue        # 건너뛴다.
+    else:               # i가 홀수면
+        print(i)        # 그 값을 출력한다.
 
 # !!!!! END of lab8_7.py !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
