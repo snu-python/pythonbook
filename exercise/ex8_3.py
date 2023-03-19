119473afcb48c70f7f8fe3467aea6d4b8b634021
Jinsoo Park <jinsoopark@Jinsooui-Mac-Studio.local>
2023-03-20 01:04:21 +0900

    Changed to f-string

--- exercise/ex8_3.py
+++ exercise/ex8_3.py
@@ -11,13 +11,13 @@ __author__ = 'Jinsoo Park'
 __version__ = '0.1'
 __email__ = 'snu.python@gmail.com'
 
-import math                                         # math 모듈을 불러온다.
+import math                               # math 모듈을 불러온다.
 
-i = 0                                               # 변수 i의 초깃값을 0으로 설정한다.
-while i < 10:                                       # i 값을 0부터 9까지 순회한다.
-    print('{}! = {}'.format(i, math.factorial(i)))  # i의 계승 값을 계산해 출력한다.
-    i += 1                                          # i에 1을 더한다.
-else:                                               # while문이 정상적으로 종료되면
-    print('종료')                                    # '종료'를 출력하고 while문을 빠져나간다.
+i = 0                                     # 변수 i의 초깃값을 0으로 설정한다.
+while i < 10:                             # i 값을 0부터 9까지 순회한다.
+    print(f'{i}! = {math.factorial(i)}')  # i의 계승 값을 계산해 출력한다.
+    i += 1                                # i에 1을 더한다.
+else:                                     # while문이 정상적으로 종료되면
+    print('종료')                          # '종료'를 출력하고 while문을 빠져나간다.
 
 # !!!!! END of ex8_3.py !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
