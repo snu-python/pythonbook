fae8ef31acd9c1398f197075efbff9f57f9c6d2b
Jinsoo Park <jinsoopark@Jinsooui-Mac-Studio.local>
2023-03-19 22:27:58 +0900

    Changed to f-string

--- exercise/ex8_1.py
+++ exercise/ex8_1.py
@@ -13,14 +13,14 @@ __email__ = 'snu.python@gmail.com'
 
 city = input('도시 이름을 입력하세요: ')
 
-if city == '서울':                                # city 값이 '서울'이면
-    size = 605                                   # 변수 size에 605를 입력한다.
-elif city == '파리':                              # city 값이 '파리'이면
-    size = 105                                   # 변수 size에 105를 입력한다.
-elif city == '에든버러':                           # city 값이 '에딘버러'이면
-    size = 264                                   # 변수 size에 264을 입력한다.
-else:                                            # 그 이외의 경우에는
-    size = 'Unknown'                             # 변수 size에 'Unknown'을 입력한다.
+if city == '서울':            # city 값이 '서울'이면
+    size = 605               # 변수 size에 605를 입력한다.
+elif city == '파리':          # city 값이 '파리'이면
+    size = 105               # 변수 size에 105를 입력한다.
+elif city == '에든버러':       # city 값이 '에딘버러'이면
+    size = 264               # 변수 size에 264을 입력한다.
+else:                        # 그 이외의 경우에는
+    size = 'Unknown'         # 변수 size에 'Unknown'을 입력한다.
 
 print(f'{city}의 면적은 {size} 평방 킬로미터입니다.')
 # print('{}의 면적은 {} 평방 킬로미터입니다.'.format(city, size))
