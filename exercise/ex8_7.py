d235da114fb7fd0e5604f605b1778463ebe4c26b
Jinsoo Park <jinsoopark@Jinsooui-Mac-Studio.local>
2023-03-20 21:40:51 +0900

    Changed to f-string

--- exercise/ex8_7.py
+++ exercise/ex8_7.py
@@ -8,19 +8,19 @@ Description...: Sample solution for exercise 8-7.
                 statement in infinite loop using random module.
 """
 __author__ = 'Jinsoo Park'
-__version__ = '0.1'
+__version__ = '0.11'
 __email__ = 'snu.python@gmail.com'
 
 
-import random                                    # random 모듈을 불러온다.
+import random                           # random 모듈을 불러온다.
 
-i = 0                                            # 변수 i의 초깃값을 0으로 설정한다.
-while True:                                      # 무한 루프로 순회한다.
-    i = random.randint(1, 9)                     # 1~9 사이의 임의의 정수를 생성해서 i에 할당한다.
-    if i <= 5:                                   # i가 5이하면
-        print(i)                                 # i를 출력하고
-        continue                                 # 다시 임의의 정수를 생성하기 위해 while문의 처음으로 돌아간다.
-    print('{}: 무한루프에서 빠져나갑니다.'.format(i))   # i가 5이하가 아니면 i를 출력하고
-    break                                        # 무한 루프를 종료한다.
+i = 0                                   # 변수 i의 초깃값을 0으로 설정한다.
+while True:                             # 무한 루프로 순회한다.
+    i = random.randint(1, 9)            # 1~9 사이의 임의의 정수를 생성해서 i에 할당한다.
+    if i <= 5:                          # i가 5이하면
+        print(i)                        # i를 출력하고
+        continue                        # 다시 임의의 정수를 생성하기 위해 while문의 처음으로 돌아간다.
+    print(f'{i}: 무한루프에서 빠져나갑니다.')   # i가 5이하가 아니면 i를 출력하고
+    break                               # 무한 루프를 종료한다.
 
 # !!!!! END of ex8_7.py !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
